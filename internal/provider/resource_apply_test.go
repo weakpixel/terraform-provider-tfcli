0c435d98c823738d3de3dde04aa3ba561c7d6e9d
waekpixel <weakpixel@gmail.com>
2022-05-04 21:15:35 +0200

    cleanup and unit tests

--- internal/provider/resource_apply_test.go
+++ internal/provider/resource_apply_test.go
@@ -1,32 +1,78 @@
 package provider
 
 import (
+	"os"
+	"path/filepath"
 	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/weakpixel/tfcli"
 )
 
-func TestAccResourceScaffolding(t *testing.T) {
-	t.Skip("resource not yet implemented, remove this once you add your own code")
+func TestAccResourceApply(t *testing.T) {
+
+	bin, err := tfcli.DownloadTerraform("1.1.9", false)
+	if err != nil {
+		t.Logf("cannot download Terraform: %s", err)
+		t.FailNow()
+	}
+	dir := filepath.Dir(bin)
+	path := os.Getenv("PATH")
+
+	os.Setenv("PATH", dir+string(os.PathListSeparator)+path)
 
 	resource.UnitTest(t, resource.TestCase{
 		PreCheck:          func() { testAccPreCheck(t) },
 		ProviderFactories: providerFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccResourceScaffolding,
+				Config: testAccResourceApplyWithTFDownload,
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestMatchResourceAttr(
-						"scaffolding_resource.foo", "sample_attribute", regexp.MustCompile("^ba")),
+						"tfcli_apply.test1", "output.string_var", regexp.MustCompile("Hello")),
+				),
+			},
+
+			{
+				Config: testAccResourceApplyTerraformFromPath,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestMatchResourceAttr(
+						"tfcli_apply.test2", "output.string_var", regexp.MustCompile("MyResult")),
 				),
 			},
 		},
 	})
 }
 
-const testAccResourceScaffolding = `
-resource "apply" "foo" {
-  sample_attribute = "bar"
+const testAccResourceApplyWithTFDownload = `
+resource "tfcli_apply" "test1" {
+	terraform_version = "1.0.0"
+	source  = "weakpixel/test-module/tfcli"
+  	version = "0.0.2"
+	vars = {
+	"string_var" = "Hello"
+	}
+}
+`
+
+const testAccResourceApplyTerraformFromPath = `
+resource "tfcli_apply" "test2" {
+	source  = "weakpixel/test-module/tfcli"
+  	version = "0.0.2"
+	vars = {
+	"string_var" = "MyResult"
+	}
+}
+`
+
+const testAccResourceApplyWithLegazyTF = `
+resource "tfcli_apply" "foo" {
+	terraform_version = "0.15.1"
+	source  = "weakpixel/test-module/tfcli"
+  	version = "0.0.2"
+	vars = {
+	"string_var" = "MyResult"
+	}
 }
 `
